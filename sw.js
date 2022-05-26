9161a7d638bbdc25c2adcdd57944b57e762761ba
Rajan Patel <rajan.n.patel.97@gmail.com>
2022-05-26 21:10:16 +0100

    added other projects

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d93d52d42a20ccbacbff.js"
+    "url": "webpack-runtime-513aa40821226133830f.js"
   },
   {
     "url": "framework-aa35e29fbb74d6100667.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e2d4f13fa8f231428fe701d83540e634"
+    "revision": "b91799c46aa03cd3c0e6a84f75908af4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-50fcc746a3b9bd6c1224.js"
